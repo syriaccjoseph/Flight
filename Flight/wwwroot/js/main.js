ac7926fa72bd704957d056eee2b91d7f4a519747
Ajith Joseph <syriaccjoseph@gmail.com>
2018-12-15 17:49:12 -0500

    require js support in new-design

---
+++ Flight/wwwroot/js/main.js
@@ -0,0 +1,11 @@
+﻿require.config({
+
+        urlArgs: "bust=v2",
+        paths: {
+               antlr4: "../lib/antlr4/index",
+               lexer: "../lib/PreferenceLanguage/PreferenceLanguageLexer",
+               parser: "../lib/PreferenceLanguage/PreferenceLanguageParser",
+               listener: "../lib/PreferenceLanguage/PreferenceLanguageListener",
+        }
+
+});
--- Flight/wwwroot/lib/antlr4/index.js
+++ Flight/wwwroot/lib/antlr4/index.js
@@ -2,12 +2,15 @@
  * Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
-exports.atn = require('./atn/index');
-exports.codepointat = require('./polyfills/codepointat');
-exports.dfa = require('./dfa/index');
-exports.fromcodepoint = require('./polyfills/fromcodepoint');
-exports.tree = require('./tree/index');
-exports.error = require('./error/index');
+
+var exports = {};
+
+exports.atn = require('../atn/index');
+exports.codepointat = require('../polyfills/codepointat');
+exports.dfa = require('../dfa/index');
+exports.fromcodepoint = require('../polyfills/fromcodepoint');
+exports.tree = require('../tree/index');
+exports.error = require('../error/index');
 exports.Token = require('./Token').Token;
 exports.CharStreams = require('./CharStreams').CharStreams;
 exports.CommonToken = require('./Token').CommonToken;
